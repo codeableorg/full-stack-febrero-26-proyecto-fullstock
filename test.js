67bbcd1876b65f7b08408c4496ad61b55a6ebe28
Andre Tavara <andretj_92@live.com>
2026-03-30 19:33:13 -0500

    Agrega primera clase de JavaScript

---
+++ test.js
@@ -0,0 +1,11 @@
+// Código JavaScript:
+// alert("Hola me llamo André");
+// const h1Element = document.querySelector("h1");
+// const mensaje = `El H1 dice: ${h1Element.textContent}`;
+// console.log(mensaje);
+
+console.log(99);
+console.log(2026);
+console.log("Hola mundo!");
+console.log("Codeable");
+console.log("100");
---
+++ test2.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo!");
+
+// Tipos de datos (categorias) de los valores (dato, informacion) en JavaScript:
+
+// Numbers: 2026, 27
+// Strings: André, Marzo, negro
+// Booleans: sí, no
+// Null
+// Undefined
+// BigInts
+// Symbols
+// Objects
+// Functions
