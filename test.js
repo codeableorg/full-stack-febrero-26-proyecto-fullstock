42d1568ac317df22c5abc1903973f5382198d5a5
Andre Tavara <andretj_92@live.com>
2026-03-31 21:02:51 -0500

    Agrega clase de variables JS

--- test.js
+++ test.js
@@ -1,11 +1,55 @@
-// Código JavaScript:
-// alert("Hola me llamo André");
-// const h1Element = document.querySelector("h1");
-// const mensaje = `El H1 dice: ${h1Element.textContent}`;
-// console.log(mensaje);
+// let year = 2026;
+// console.log("Codeable");
+// console.log(year);
+// let fruta = "manzana";
+// year = 2027;
+// fruta = "naranja";
+// console.log(year);
+// console.log(fruta);
 
-console.log(99);
-console.log(2026);
-console.log("Hola mundo!");
-console.log("Codeable");
-console.log("100");
+// let numero = 10;
+// let suma = 5 + 3;
+// console.log(suma);
+// let producto = numero * suma;
+// console.log(producto);
+// let resta = producto - 30;
+
+// let a = 100;
+// let b = a;
+// a = 10;
+// console.log(b);
+// console.log(a);
+// b = 99;
+// console.log(b);
+
+// const escuela = "Codeable";
+// console.log(escuela);
+// escuela = "Codeable Pro";
+
+// let c = 30;
+// let C = 40;
+
+// let x = 100;
+// x = x + 50;
+// let y = x + 200;
+// y = y + x;
+// x = x - y;
+// console.log(x);
+// console.log(y);
+
+// nombre*
+// email*
+// password*
+// numero tarjeta
+// fecha expiracion
+// codigo
+
+let nombre = "André";
+const email = "andre@mail.com";
+let password = "123456";
+let numeroTarjeta = null;
+let fechaExpiracionTarjeta = null;
+let codigoTarjeta = null;
+numeroTarjeta = 4463834628392647;
+fechaExpiracionTarjeta = "10/08/2030";
+codigoTarjeta = 678;
--- test2.js
+++ test2.js
@@ -3,11 +3,14 @@ console.log("Hola Mundo!");
 // Tipos de datos (categorias) de los valores (dato, informacion) en JavaScript:
 
 // Numbers: 2026, 27
-// Strings: André, Marzo, negro
-// Booleans: sí, no
-// Null
+// Strings: "André", "Marzo", "negro"
+// Booleans: true, false
+
+// Null: null
 // Undefined
+
 // BigInts
 // Symbols
+
 // Objects
 // Functions
