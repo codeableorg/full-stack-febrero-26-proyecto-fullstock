ceaedce99c21f503e1176c29fea34c5da2db254f
Andre Tavara <andretj_92@live.com>
2026-04-16 21:02:38 -0500

    Agrega la clase de switch y for

---
+++ bucles.js
@@ -0,0 +1,8 @@
+// for
+// una iteracion es cada una de las veces que se ejecuta el bloque de codigo
+// mientras i sea menor a 5
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+console.log("Fin del programa!");
